7c7d4f2dbdfce369c3851af27fcaecdd958b2797
nazi sharifi <banonazisharifi@gmail.com>
2026-04-30 17:17:22 +0430

    add relation

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -9,4 +9,10 @@ class Teacher extends Model
     public function user(){
         return $this->belongsTo(User::class , 'user_id');
     }
+    public function salairy(){
+        return $this->hasMany(salarie::class , 'teacher_id');
+    }
+    public function sinf(){
+        return $this->hasMany(sinf::class , 'teacher_di');
+    }
 }
