cc4b4076d18ec52eeb09c17146623aa3fe0c8c0f
nazi sharifi <banonazisharifi@gmail.com>
2026-04-30 17:23:48 +0430

    add relation

--- app/Models/sinf.php
+++ app/Models/sinf.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class sinf extends Model
 {
-    //
+    public function teacher(){
+        return $this->belongsTo(sinf::class , 'teacher_id');
+    }
+    public function payment(){
+        return $this->hasMany(payment::class , 'sinf_id');
+    }
 }
