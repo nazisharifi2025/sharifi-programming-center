02f5617d8cb7fd873b1ce34a41ce9bb03100ff04
nazi sharifi <banonazisharifi@gmail.com>
2026-04-30 17:20:59 +0430

    add relation

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo(user::class , 'user_id');
+    }
+    public function payment(){
+        return $this->hasMany(payment::class , 'student_id');
+    }
 }
