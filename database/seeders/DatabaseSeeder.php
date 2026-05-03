7af0ad4de6bfa4437ddc3f0d7d8dec990e147775
nazi sharifi <banonazisharifi@gmail.com>
2026-05-03 22:46:05 +0430

    add sinfseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call([
-            AdminSeeder::class,
-            StudentSeeder::class,
-            TeacherSeeder::class,
+           sinfSeeder::class,
         ]);
     }
 }
