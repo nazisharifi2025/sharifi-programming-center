697ba30850a0ea470835e42c8011385891cb02b1
nazi sharifi <banonazisharifi@gmail.com>
2026-05-17 22:50:58 +0430

    add relation ship

--- app/Livewire/Sinf/ListSinfs.php
+++ app/Livewire/Sinf/ListSinfs.php
@@ -31,7 +31,7 @@ class ListSinfs extends Component implements HasActions, HasSchemas, HasTable
             ->columns([
                 TextColumn::make('title')->label('Cource Name'),
                 TextColumn::make('teacher.user.name')->label('Name')->sortable()->searchable(),
-                TextColumn::make('students.user.name'),
+                TextColumn::make('payment.student.user.name'),
                 TextColumn::make('start_date'),
                 TextColumn::make('end_date'),
                 TextColumn::make('description')->limit(25),
