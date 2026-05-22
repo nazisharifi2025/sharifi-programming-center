63faf9e899ad37508ed9be7eb462fd2ec58f243f
nazi sharifi <banonazisharifi@gmail.com>
2026-05-22 21:44:43 +0430

    add new edit form for sinfs

---
+++ app/Livewire/Sinf/editsinf.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Livewire\Sinf;
+
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
+use Filament\Schemas\Concerns\InteractsWithSchemas;
+use Filament\Schemas\Contracts\HasSchemas;
+use Filament\Schemas\Schema;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+use sinf;
+
+class editsinf extends Component implements HasActions, HasSchemas
+{
+    use InteractsWithActions;
+    use InteractsWithSchemas;
+
+    public sinf $record;
+
+    public ?array $data = [];
+
+    public function mount(): void
+    {
+        $this->form->fill($this->record->attributesToArray());
+    }
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ])
+            ->statePath('data')
+            ->model($this->record);
+    }
+
+    public function save(): void
+    {
+        $data = $this->form->getState();
+
+        $this->record->update($data);
+    }
+
+    public function render(): View
+    {
+        return view('livewire.sinf.editsinf');
+    }
+}
