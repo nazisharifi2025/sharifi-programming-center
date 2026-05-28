e2bc80e29f7f617e7fada11c878a21f63dce729e
nazi sharifi <banonazisharifi@gmail.com>
2026-05-28 22:20:42 +0430

    add component ading sinf

--- app/Livewire/Student/CreateStudent.php
+++ app/Livewire/Student/CreateStudent.php
@@ -9,7 +9,8 @@ use Filament\Schemas\Contracts\HasSchemas;
 use Filament\Schemas\Schema;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
-use App\Model\Student;
+use App\Models\Student;
+use Filament\Forms\Components\TextInput;
 
 class CreateStudent extends Component implements HasActions, HasSchemas
 {
