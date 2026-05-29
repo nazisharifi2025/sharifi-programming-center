cb5b731fc56e6218bf673a8227c06e57b0e62caf
nazi sharifi <banonazisharifi@gmail.com>
2026-05-29 20:28:39 +0430

    add new form

---
+++ app/Livewire/Payment/CreatePayment.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Livewire\Payment;
+
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
+use Filament\Schemas\Concerns\InteractsWithSchemas;
+use Filament\Schemas\Contracts\HasSchemas;
+use Filament\Schemas\Schema;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+use payment;
+
+class CreatePayment extends Component implements HasActions, HasSchemas
+{
+    use InteractsWithActions;
+    use InteractsWithSchemas;
+
+    public ?array $data = [];
+
+    public function mount(): void
+    {
+        $this->form->fill();
+    }
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ])
+            ->statePath('data')
+            ->model(payment::class);
+    }
+
+    public function create(): void
+    {
+        $data = $this->form->getState();
+
+        $record = payment::create($data);
+
+        $this->form->model($record)->saveRelationships();
+    }
+
+    public function render(): View
+    {
+        return view('livewire.payment.create-payment');
+    }
+}
